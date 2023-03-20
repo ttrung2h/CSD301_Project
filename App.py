7aa35fd0170c331b088b840d59ff0e40e7c4ec5b
Hoang Trung <hoangtrungcongviec@gmail.com>
2023-03-20 15:45:43 +0700

    add update plot

--- App.py
+++ App.py
@@ -3,48 +3,61 @@ from Plot import plot
 from InputData import InputData
 from Valid import Valid
 if __name__ == "__main__":
-    number_of_nodes = InputData.input_number_nodes()
+    number_of_nodes = 9 #InputData.input_number_nodes()
     
-    # create matrix 2 dimension have length = number_of_nodes
-    matrix = [[0 for i in range(number_of_nodes)] for j in range(number_of_nodes)]
+    # # create matrix 2 dimension have length = number_of_nodes
+    # matrix = [[0 for i in range(number_of_nodes)] for j in range(number_of_nodes)]
     G = Graph(number_of_nodes)
     
-    # loop input edge
-    while True:
-        #input node1
-        node1 = InputData.input_node("Enter the node1: ", number_of_nodes)
-        node1 = ord(node1) - 65
+    # # loop input edge
+    # while True:
+    #     #input node1
+    #     node1 = InputData.input_node("Enter the node1: ", number_of_nodes)
+    #     node1 = ord(node1) - 65
         
-        #input node2
-        node2 = InputData.input_node("Enter the node2: ", number_of_nodes)
-        node2 = ord(node2) - 65
+    #     #input node2
+    #     node2 = InputData.input_node("Enter the node2: ", number_of_nodes)
+    #     node2 = ord(node2) - 65
         
-        #input weight
-        weight = InputData.input_weight()
+    #     #input weight
+    #     weight = InputData.input_weight()
         
-        # Check edge already exists
-        if matrix[node1][node2] != 0 and matrix[node2][node1] != 0:
-            answer = InputData.input_yes_no("Edge already exists. Do you want to replace? (Y/N): ")
-            if not Valid.check_yes_no(answer):
-                continue
+    #     # Check edge already exists
+    #     if matrix[node1][node2] != 0 and matrix[node2][node1] != 0:
+    #         answer = InputData.input_yes_no("Edge already exists. Do you want to replace? (Y/N): ")
+    #         if not Valid.check_yes_no(answer):
+    #             continue
         
-        matrix[node1][node2],matrix[node2][node1] = weight,weight
-        G.add_edge(node1, node2, weight)
+    #     matrix[node1][node2],matrix[node2][node1] = weight,weight
+    #     G.add_edge(node1, node2, weight)
         
-        # Check graph fully connected
-        if Valid.is_connected(matrix):
-            answer = InputData.input_yes_no("Graph is connected. Do you want to continue? (Y/N): ")
-            if not Valid.check_yes_no(answer):
-                break
+    #     # Check graph fully connected
+    #     if Valid.is_connected(matrix):
+    #         answer = InputData.input_yes_no("Graph is connected. Do you want to continue? (Y/N): ")
+    #         if not Valid.check_yes_no(answer):
+    #             break
         
-        
-    
+    ## Test    
+    number_of_nodes = 9
+    G.add_edge(2, 4, 6)
+    G.add_edge(1,4,10)
+    G.add_edge(2,5,15)
+    G.add_edge(6,8,17)
+    G.add_edge(6,7,18)
+    G.add_edge(5,6,19)
+    G.add_edge(0,2,20)
+    G.add_edge(5,7,20)
+    G.add_edge(7,8,20)
+    G.add_edge(0,3,21)
+    G.add_edge(4,6,23)
+    G.add_edge(0,1,25)
+    G.add_edge(3,5,25)
     # show matrix
-    print(matrix)
+    # print(matrix)
     original_graph = G.get_orignal_graph()
     spanning_graph = G.kruskals_mst()
     print(original_graph)
     print(spanning_graph)
-    show_graph = plot(original_graph, spanning_graph)
+    show_graph = plot(number_of_nodes,original_graph, spanning_graph)
     show_graph.plot()
     
--- Plot.py
+++ Plot.py
@@ -1,14 +1,15 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-
+import time
 class plot:
-    def __init__(self, original_graph,spanning_graph):
+    def __init__(self,num_of_nodes, original_graph,spanning_graph):
+        self.num_of_nodes = num_of_nodes
         self.original_graph = original_graph
         self.spanning_graph = spanning_graph
 
     def plot(self):
-        plt.figure(figsize=(5, 10))
-
+        
+        
         # Define the original graph
         G = nx.Graph()
         for node1,node2,weight in self.original_graph:
@@ -16,27 +17,25 @@ class plot:
             node1, node2 = chr(node1+65), chr(node2+65)
             G.add_edge(node1, node2, weight=int(weight))
 
+        fig = plt.figure(figsize=(10,12))
+        # Draw the graph with labels
+        pos = nx.spring_layout(G)
         
         # Graph spanning tree
         T = nx.Graph()
-        for node1,node2,weight in self.spanning_graph:
+        for i,(node1,node2,weight) in enumerate(self.spanning_graph):
+            # Draw the original graph
+            nx.draw_networkx_edges(G, pos, edge_color='gray', alpha=0.5)
+            nx.draw_networkx_nodes(G, pos, node_size=200)
+            nx.draw_networkx_labels(G, pos, font_size=12, font_family='sans-serif')
+            nx.draw_networkx_edge_labels(G, pos, edge_labels={(u, v): d['weight'] for u, v, d in G.edges(data=True)}, label_pos=0.4, font_size=10)
             # convert digital node to letter
             node1, node2 = chr(node1+65), chr(node2+65)
+            
+            # Draw the minimum spanning tree
             T.add_edge(node1, node2, weight=int(weight))
-        
-
-        # Draw the graph with labels
-        pos = nx.spring_layout(G)
-        # Draw the original graph
-        nx.draw_networkx_edges(G, pos, edge_color='gray', alpha=0.5)
-        nx.draw_networkx_nodes(G, pos, node_size=200)
-        nx.draw_networkx_labels(G, pos, font_size=12, font_family='sans-serif')
-        nx.draw_networkx_edge_labels(G, pos, edge_labels={(u, v): d['weight'] for u, v, d in G.edges(data=True)}, label_pos=0.4, font_size=10)
-
-        # Draw the minimum spanning tree
-        nx.draw_networkx_edges(T, pos, edge_color='red', alpha=1.0, width=2)
-        nx.draw_networkx_nodes(T, pos, node_size=200, node_color='red')
-        nx.draw_networkx_edge_labels(T, pos, edge_labels={(u, v): d['weight'] for u, v, d in G.edges(data=True)}, label_pos=0.4, font_size=10)
-        # Show the plot
-        plt.axis('off')
-        plt.show()
+            nx.draw_networkx_edges(T, pos, edge_color='red', alpha=1.0, width=2)
+            nx.draw_networkx_nodes(T, pos, node_size=200, node_color='red')
+            nx.draw_networkx_edge_labels(T, pos, edge_labels={(u, v): d['weight'] for u, v, d in G.edges(data=True)}, label_pos=0.4, font_size=10)
+            plt.title(f'Step {i}')
+            plt.show()
